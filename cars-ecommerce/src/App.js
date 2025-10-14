508ca168a1c4d10c0f89190bf971ffb3aa281b7d
Juma Michael <obudho76@gmail.com>
2025-10-14 11:18:37 +0300

    implemented app with all raoutes

--- cars-ecommerce/src/App.js
+++ cars-ecommerce/src/App.js
@@ -1,1 +1,23 @@
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./page/Home";
+import CompareCars from "./page/CompareCars";
+import Gallery from "./page/Gallery";
+import Contact from "./page/Contact";
+
+export default function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/compare-cars" element={<CompareCars />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
---
+++ cars-ecommerce/src/page/Home.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <div>
+        <h1>Welcome to the Car E-commerce Home Page</h1>
+        <p>Explore our collection of cars and find your perfect match!</p>
+        </div>
+        
+    )
+    }
